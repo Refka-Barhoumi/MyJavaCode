5ec717823c16409ac3958f336cacdc80052fe25d
Refkacs50 <barhoumirefka@fstsbz.u-kairouan.tn>
2025-10-12 10:47:41 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=2;
+	    //Affichage avec retour à la ligne
+		System.out.println("Valeur de a: "+a);
+		//Affichage sur la même ligne
+		System.out.print("Valeur de a: "+a);
+		System.out.println("coucou");
+		int age=12;
+		System.out.println(age);//Affiche la valeur de la variable age
+		int poids=33;
+		System.out.println("L'age de la personne est "+age+", son poids: "+poids+" Kg");
+		//Affiche l'age de la personne est 12, son poids: 33 Kg
+	}
+}
